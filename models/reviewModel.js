00208c896ae1a38505555ae20f0e518172323c69
Arghya <arghya.official@yahoo.com>
2025-01-05 20:43:32 +0530

    finished section 11

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,7 @@
 // review / rating / createdAt / ref to tour / ref to user
 
 const mongoose = require('mongoose');
+const tourModel = require('../models/tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -36,6 +37,13 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index(
+  { tour: 1, user: 1 },
+  {
+    unique: true
+  }
+);
+
 reviewSchema.pre(/^find/, function(next) {
   this.populate({
     path: 'user',
@@ -50,6 +58,42 @@ reviewSchema.pre(/^find/, function(next) {
   //   });
 
   next();
+});
+
+reviewSchema.statics.calcAverageRating = async function(tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: '$tour',
+        nReviews: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  console.log(stats);
+
+  await tourModel.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nReviews || 0,
+    ratingsAverage: stats[0].avgRating || 0
+  });
+};
+
+// Calculating avg rating after a new review is posted on a tour
+reviewSchema.post('save', function() {
+  this.constructor.calcAverageRating(this.tour);
+});
+
+// Calculating avg rating after a review is updated or Deleted
+
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  this.r = await this.findOne();
+  console.log(this.r);
+  next();
+});
+reviewSchema.post(/^findOneAnd/, async function(next) {
+  await this.r.constructor.calcAverageRating(this.r.tour);
 });
 
 module.exports = mongoose.model('Review', reviewSchema);
