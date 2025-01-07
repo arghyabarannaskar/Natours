c5dbd804a7bad806492361ff51d45a6250176d93
Arghya <arghya.official@yahoo.com>
2025-01-07 13:56:38 +0530

    added new code

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -18,12 +20,27 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 // serving satic files
+app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // Frontend origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true // Allow cookies if needed
+  })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 1) GLOBAL MIDDLEWARES
 // Set security HTTP headers
 app.use(helmet());
-
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      connectSrc: ["'self'", 'http://127.0.0.1:3000'] // Allow API requests to backend
+    }
+  })
+);
 // Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -40,6 +57,7 @@ app.use('/api', limiter);
 
 // Parse JSON request bodies (for POST, PUT, PATCH requests)
 app.use(express.json({ limit: '10kb' })); // req.body larger than 10kb will be rejected
+app.use(cookieParser());
 
 // Data sanitization again NoSQL query injection
 app.use(mongoSanitize());
